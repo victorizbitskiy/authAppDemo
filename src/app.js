717da007288ce97d941fb587519ca48e7e5b9561
victorizbitskiy <victor.izbitskiy@yandex.ru>
2021-10-19 19:17:08 +0300

    added submitBtn validation

--- src/app.js
+++ src/app.js
@@ -1,3 +1,30 @@
+import { isValid } from '../utils'
 import './styles.css'
 
-console.log('App working')
+const form = document.getElementById('form')
+const input = form.querySelector('#question-input')
+const submitBtn = form.querySelector('#submit')
+
+form.addEventListener('submit', submitFormHandler)
+input.addEventListener('input', () => {
+  submitBtn.disabled = !isValid(input.value)
+})
+
+function submitFormHandler(event) {
+  event.preventDefault() // чтобы страница не перезагружалась
+
+  if (isValid(input.value)) {
+    const question = {
+      text: input.value.trim(),
+      date: new Date().toJSON()
+    }
+
+    submitBtn.disabled = true
+    // Async request to server to save question
+    console.log('Question', question)
+    input.value = ''
+    input.className = ''
+    submitBtn.disabled = false
+
+  }
+}
